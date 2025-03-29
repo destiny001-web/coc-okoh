144637a91e960882ade0b02439aea1c120626f64
destiny001-web <omorudestinyjohn@gmail.com>
2025-03-29 01:31:13 -0700

    invitation

--- app/src/components/Main/others/Invitation.svelte
+++ app/src/components/Main/others/Invitation.svelte
@@ -1,8 +1,8 @@
 
 
-<section class="grid place-items-center gap-10 px-5 @3xl:grid-cols-2 @3xl:text-2xl">
+<section class="grid place-items-center gap-10 px-5 3xl:grid-cols-2 3xl:text-2xl">
     <div class="text-center flex flex-col items-center gap-5">
-        <p>Sermon</p>
+        <p class="font-bold text-2xl">Sermon</p>
         <p>Join us in spreading God's love through community outreach, charity, and missions. Be the hands and feet of Christ in a world that needs hope. As we utilize the various evangelism methods. You can participate by sending us a message.</p>
         <p>Join us in spreading God's love through community outreach, charity, and missions. Be the hands and feet of Christ in a world that needs hope. As we utilize the various evangelism methods. You can participate by sending us a message.</p>
         <p>Join us in spreading God's love through community outreach, charity, and missions. Be the hands and feet of Christ in a world that needs hope. As we utilize the various evangelism methods. You can participate by sending us a message.</p>
--- app/src/routes/(view)/home/+page.svelte
+++ app/src/routes/(view)/home/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+	import Invitation from "../../../components/Main/others/Invitation.svelte";
 	import Testimonies from "../../../components/Main/others/Testimonies.svelte";
 
 </script>
+<Invitation />
 <Testimonies />
